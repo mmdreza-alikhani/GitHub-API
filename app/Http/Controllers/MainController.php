0a168f343398ffd28db1000ceb4260e7150461c6
MmdReza Alikhani <mmdreza.alikhani@gmail.com>
2024-10-31 01:19:29 +0330

    add helpers.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Repository;
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Support\Facades\Http;
 
@@ -17,7 +18,14 @@ class MainController
         $response = Http::withToken('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')->get('https://api.github.com/users/' . $user->name . '/starred');
 
         foreach ($response->object() as $repo){
-            dd($repo->language);
+            Repository::create([
+                'user_id' => auth()->id(),
+                'name' => $repo->name,
+                'repository_id' => $repo->id,
+                'language' => $repo->language,
+                'url' => $repo->url,
+                'description' => 'nuthing',
+            ]);
         }
     }
 }
--- app/Models/Repository.php
+++ app/Models/Repository.php
@@ -3,19 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Repository
+class Repository extends Model
 {
     use HasFactory, SoftDeletes;
     protected $table = 'repositories';
-    protected $fillable = [
-        'user_id',
-        'repository_id',
-        'name',
-        'language',
-        'url',
-        'description',
-    ];
+    protected $guarded = [];
 
 }
